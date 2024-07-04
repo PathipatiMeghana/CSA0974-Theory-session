b647d1b7b72f3d29618a69b0501a31e356a3adca
PathipatiMeghana <130462039+PathipatiMeghana@users.noreply.github.com>
2024-07-04 22:11:39 +0530

    Add files via upload

---
+++ Overload.java
@@ -0,0 +1,24 @@
+ class Overload3 {
+    void add(int a, int b) {
+        int c = a + b;
+        System.out.println("This is add(int a, int b): " + c);
+    }
+    void add(float a, float b) {
+        float c = a + b;
+        System.out.println("This is add(float a, float b): " + c);
+    }
+    void add(int a, float b) {
+        float c = a + b;
+        System.out.println("This is add(int a, float b): " + c);
+    }
+    void add(float a, int b) {
+        float c = a + b;
+        System.out.println("This is add(float a, int b): " + c);
+    }
+
+    public static void main(String[] args) {
+        Overload3 o1 = new Overload3();
+        o1.add(2, 3);        
+        o1.add(2.7f, 3.5f);  
+        
+}
